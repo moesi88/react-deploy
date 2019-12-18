353722eb3f949f1501a69486d749ca73a43d6551
Maher Kurdi <m.saadkrde@ltp.com>
2019-12-18 14:19:07 +0100

    Updates

---
+++ precache-manifest.485fc9ef23cc7bdfcb718017ca4339bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c516affad86d2ef9e0cb194058aee3b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "80c2c94e55c7f57a7f28",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be282bae84d9dd2b6237",
+    "url": "/react-deploy/static/js/2.649e0b91.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deploy/static/js/2.649e0b91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80c2c94e55c7f57a7f28",
+    "url": "/react-deploy/static/js/main.adb053d8.chunk.js"
+  },
+  {
+    "revision": "1d020b3c296b9ed426af",
+    "url": "/react-deploy/static/js/runtime-main.3475fb37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
